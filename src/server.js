53fb58b20a51635a34fa176297d9f40739d4b91b
Simon Auner <simonauner@gmail.com>
2019-03-01 15:01:50 +0100

    move css to head to avoid FOUC

--- src/server.js
+++ src/server.js
@@ -30,14 +30,15 @@ function html() {
         <head>
             <meta charset="utf-8">
             <title>Video page</title>
+
+            <link href="https://mrgreentech.github.io/pam/kss-assets/css/pam.css" rel="stylesheet">
+            <link href="/app.css" rel="stylesheet">
         </head>
         
         <body>
             <div id="app"></div>
-            <script src="/client.js"></script>
+            <script src="/client.js" async></script>
 
-            <link href="https://mrgreentech.github.io/pam/kss-assets/css/pam.css" rel="stylesheet">
-            <link href="/app.css" rel="stylesheet">
         </body>
         </html>
     `;
